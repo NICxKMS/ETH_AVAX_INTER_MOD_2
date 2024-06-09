8cae3679ae6cbdd5c0f9362941da55943590c221
Nikhil Kumar <121129363+NICxKMS@users.noreply.github.com>
2024-06-09 12:12:38 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,73 +1,70 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
-contract Assessment {
+contract GameToken {
     address payable public owner;
     uint256 public balance;
-    string  public _tokenName = "NIKHIL KUMAR";
-    string  public _tokenAbbrv = "NK";
-    uint256 public  _totalSupply = 0;
+    string public tokenName = "NIKHIL KUMAR";
+    string public tokenAbbrv = "NK";
+    uint256 public totalSupply;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner of this account");
+        _;
+    }
 
     constructor() payable {
         owner = payable(msg.sender);
-        // balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns(uint256) {
         return balance;
     }
+
     function getTokenName() public view returns(string memory) {
-        return _tokenName;
+        return tokenName;
     }
 
     function getTokenAbbrv() public view returns(string memory) {
-        return _tokenAbbrv;
+        return tokenAbbrv;
     }
 
-    function getTotal() public view returns(uint256) {
-        return _totalSupply;
+    function getTotalSupply() public view returns(uint256) {
+        return totalSupply;
     }
-    function mint(uint256 _amount) public payable {
-        uint _previousBalance = balance;
 
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
+    function recharge(address _address, uint256 _amount) public onlyOwner {
+        uint256 previousBalance = balance;
+        balances[_address] += _amount;
+        totalSupply += _amount;
         balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+        assert(balance == previousBalance + _amount);
     }
 
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+    function redeem(address _address, uint256 _amount) public {
+        require(_amount <= balances[_address], "Insufficient balance to redeem");
+        uint256 previousBalance = balance;
+        balances[_address] -= _amount;
+        totalSupply -= _amount;
+        balance -= _amount;
+        assert(balance == previousBalance - _amount);
+    }
 
-    function burn(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
+    function redeemItem(address _address, uint256 _amount, string memory _item) public {
+        require(_amount <= balances[_address], "Insufficient balance to redeem item");
+        uint256 previousBalance = balance;
+        balances[_address] -= _amount;
+        totalSupply -= _amount;
+        balance -= _amount;
+        assert(balance == previousBalance - _amount);
+    }
+
+    function changeOwner(address _newOwner) public onlyOwner {
+        if (msg.sender != owner) {
+            revert("Requires owner to personally change to new owner");
         }
-
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+        owner = payable(_newOwner);
     }
+
+    mapping(address => uint256) public balances;
 }
